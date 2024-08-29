c29cce16c63dcc51c87b47ff53dbe6e5878ecd18
N7331 <shadowkaster369@proton.me>
2024-08-30 05:13:35 +1000

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('clickMeButton');
+    button.addEventListener('click', () => {
+        alert('Button clicked!');
+    });
+});
